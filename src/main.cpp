7bad5b24fecd9e3ac6e56036e5132614316440e2
Florian Rappl <florian.rappl@physik.uni-regensburg.de>
2014-12-11 16:48:45 +0100

    Improved help

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,11 @@
 using namespace std;
 
 void setup(CmdParser& parser) {
-	parser.set_optional<string>("o", "output", "data.out", "Path where the measurements will be stored.");
+	parser.set_optional<string>("o", "output", "data.out", "Name of the file that will be used for output.");
 	parser.set_optional<int>("n", "nt", 100, "The number of points in temporal direction.");
-	parser.set_optional<double>("w", "omegasq", 1.0, "The value of omega², which is proportional to a.");
-	parser.set_optional<double>("t", "tau", 1.0, "The precision of the integrator, since ε = τ / N_delta.");
-	parser.set_optional<int>("d", "ndelta", 10, "The number of steps in the integrator, called N_delta.");
+	parser.set_optional<double>("w", "omegasq", 1.0, "The value of the coupling ω². It is ω ~ a.");
+	parser.set_optional<double>("t", "tau", 1.0, "The trajectory length in molecular dynamics time, where ε = τ / nsteps.");
+	parser.set_optional<int>("l", "nsteps", 10, "The number of leapfrog steps per trajectory.");
 	parser.set_optional<int>("m", "nmeas", 1000, "The number of steps, i.e. the number of measurements.");
 	parser.set_optional<int>("i", "ninit", 100, "The number of steps in the thermalization process.");
 	parser.set_optional<int>("s", "seed", 0, "The seed for the random number generator.");
@@ -63,7 +63,7 @@ int main(int argc, char** argv) {
 		cmd.get<int>("m"),
 		cmd.get<int>("i"),
 		cmd.get<double>("t"),
-		cmd.get<int>("d"),
+		cmd.get<int>("l"),
 		cmd.get<int>("s")
 	};
 
